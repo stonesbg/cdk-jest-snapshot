d602f8582ee9b89bc9374945896522a28aa46de0
Brian Garavey <stonesbg@gmail.com>
2025-05-25 17:39:19 +0000

    cleanup files add jest snapshot

--- README.md
+++ README.md
@@ -1,7 +1,8 @@
-# cdk-vitest-snapshot
+# cdk-jest-snapshot
 
 Purpose of this repo is to test a few key concepts:
 
-- utilization of [vitest](https://vitest.dev/) with cdk instead of using Jest
-- usage of snapshot through vitest but also using snapshot serializers in order to control the output from serialization
+- utilization of jest with cdk instead of using Jest
+- usage of snapshot through jest but also using snapshot serializers in order to control the output from serialization
 - Introductions of [Nx](https://nx.dev/)
+# cdk-jest-snapshot
---
+++ infra/sample/test/add.ts
@@ -0,0 +1,17 @@
+export function addAssetSnapshotSerializer(): void {
+  const bucketMatch = new RegExp(`cdk-[0-9a-z]{9}-assets`);
+  const assetMatch = /[0-9a-f]{64}\.(zip|json)/;
+
+  expect.addSnapshotSerializer({
+    test: (val) =>
+      typeof val === "string" &&
+      (val.match(bucketMatch) != null || val.match(assetMatch) != null),
+    print: (val) => {
+      // Substitute both the bucket part and the asset zip part
+      let sval = `${val}`;
+      sval = sval.replace(bucketMatch, "[ASSET BUCKET]");
+      sval = sval.replace(assetMatch, "[ASSET ZIP]");
+      return `"${sval}"`;
+    },
+  });
+}
